e50838cb89cda59f39a848655f770ffe5543de9d
Daniel Oliveira <danmiranda227@gmail.com>
2026-01-24 18:13:23 +0000

    feat add chakra tooltips to the skill component

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,7 +1,8 @@
-import { Card, CardBody } from '@chakra-ui/react';
+import { Card, CardBody, Tooltip } from '@chakra-ui/react';
 import {
   SiAzuredevops,
   SiDiscord,
+  SiGithubcopilot,
   SiMacos,
   SiPostman,
   SiVisualstudio,
@@ -15,38 +16,41 @@ interface Props {
 
 const Skill = ({ skill }: Props) => {
   return (
-    <Card
-      title={skill}
-      className="simple-card"
-      variant="elevated"
-      size="sm"
-      borderColor="brand.500"
-    >
-      <CardBody>
-        {(() => {
-          switch (skill) {
-            case 'VS':
-              return <SiVisualstudio size="30px" />;
-            case 'Postman':
-              return <SiPostman size="30px" />;
-            case 'Windows':
-              return <SiWindows size="30px" />;
-            case 'Discord':
-              return <SiDiscord size="30px" />;
-            case 'Mac':
-              return <SiMacos size="30px" />;
-            case 'StackOverflow':
-              return <TbBrandStackoverflow size="30px" />;
-            case 'Git':
-              return <TbBrandGit size="30px" />;
-            case 'Azure':
-              return <SiAzuredevops size="30px" />;
-            default:
-              break;
-          }
-        })()}
-      </CardBody>
-    </Card>
+    <Tooltip label={skill} hasArrow>
+      <Card
+        className="simple-card"
+        variant="elevated"
+        size="sm"
+        borderColor="brand.500"
+      >
+        <CardBody>
+          {(() => {
+            switch (skill) {
+              case 'Visual Studio / VS Code':
+                return <SiVisualstudio size="30px" />;
+              case 'Git':
+                return <TbBrandGit size="30px" />;
+              case 'GitHub Copilot':
+                return <SiGithubcopilot size="30px" />;
+              case 'Azure':
+                return <SiAzuredevops size="30px" />;
+              case 'Postman':
+                return <SiPostman size="30px" />;
+              case 'Windows':
+                return <SiWindows size="30px" />;
+              case 'Mac':
+                return <SiMacos size="30px" />;
+              case 'StackOverflow':
+                return <TbBrandStackoverflow size="30px" />;
+              case 'Discord':
+                return <SiDiscord size="30px" />;
+              default:
+                break;
+            }
+          })()}
+        </CardBody>
+      </Card>
+    </Tooltip>
   );
 };
 
